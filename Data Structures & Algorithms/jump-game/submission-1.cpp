a4ebc1f85446589b60ed39074d67b311e8029107
Parkirat Sandhu <85805976+ParkiratS@users.noreply.github.com>
2026-08-17 16:19:26 -0400

    Add: jump-game - submission-1

---
+++ Data Structures & Algorithms/jump-game/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+
+    bool canJump(vector<int>& nums) {
+        int index = nums.size()-1;
+
+        for(int i{index-1}; i>=0; i--){
+            if(i+nums[i] >= index)
+                index = i;
+        }
+
+        return (index == 0);
+        
+    }
+};
