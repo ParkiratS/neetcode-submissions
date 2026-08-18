be3e9a0666fca9a561dc0648d922bb5e370a640b
Parkirat Sandhu <85805976+ParkiratS@users.noreply.github.com>
2026-08-18 12:01:13 -0400

    Add: permutations - submission-6

---
+++ Data Structures & Algorithms/permutations/submission-6.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+
+    void permuting(vector<int>& nums, vector<vector<int>>& ans, vector<int>& curr, unordered_set<int>& seen, int index){
+        if(index >= nums.size() || index < 0 || seen.find(nums[index]) != seen.end())
+            return;
+
+        
+
+        curr.push_back(nums[index]);
+        seen.insert(nums[index]);
+
+        if(curr.size() == nums.size()){
+            ans.push_back(curr);
+            curr.pop_back();
+            seen.erase(nums[index]);
+            return;
+        }
+
+        for(int i{index+1}; i<nums.size(); i++)
+            permuting(nums, ans, curr, seen, i);
+
+        for(int i{index-1}; i>=0; i--)
+            permuting(nums, ans, curr, seen, i);
+        
+        curr.pop_back();
+        seen.erase(nums[index]);
+        if(curr.size() == 0)
+            permuting(nums, ans, curr, seen, index + 1);
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        unordered_set<int> seen;
+        vector<vector<int>> ans;
+        vector<int> curr;
+
+        permuting(nums, ans, curr, seen, 0);
+        return ans;
+    }
+};
