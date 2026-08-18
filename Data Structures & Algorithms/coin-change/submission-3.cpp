7ef838a81814a77ed079a835d8c6bbfaaca4a27f
Parkirat Sandhu <85805976+ParkiratS@users.noreply.github.com>
2026-08-18 15:24:26 -0400

    Add: coin-change - submission-3

---
+++ Data Structures & Algorithms/coin-change/submission-3.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void traverse(vector<int>& coins, vector<int>& ans, int num){
+        if(num == ans.size()-1)
+            return;
+
+        if(ans[num] != -1){
+            for(int c:coins){
+                if(num + c <= ans.size()-1){
+                    if(ans[num+c] != -1)
+                        ans[num + c] = min(ans[num]+1, ans[num+c]);
+                    else
+                        ans[num+c] = ans[num] + 1;
+                }
+            }
+        }
+
+        traverse(coins, ans, num+1);
+    }
+
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> ans(amount+1, -1);
+        ans[0] = 0;
+
+        traverse(coins, ans, 0);
+        return ans[amount];
+    }
+};
