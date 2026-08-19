dcaf6265cdec9a9cbc9beee7a58fa89beed0862d
Parkirat Sandhu <85805976+ParkiratS@users.noreply.github.com>
2026-08-19 11:15:09 -0400

    Add: valid-tree - submission-9

---
+++ Data Structures & Algorithms/valid-tree/submission-9.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+
+    bool dfs(unordered_set<int>& seen, unordered_map<int, vector<int>>& mapping, int prev, int curr){
+        if(seen.find(curr) != seen.end())
+            return false;
+
+        seen.insert(curr);
+        for(const int& e : mapping[curr]){
+            if(e != prev && !dfs(seen, mapping, curr, e))
+                return false;
+                
+        }
+
+        return true;
+
+    }
+
+    bool validTree(int n, vector<vector<int>>& edges) {
+        unordered_set<int> seen;
+        unordered_map<int, vector<int>> mapping;
+
+        for(const auto& e : edges){
+            if(mapping.find(e[0]) == mapping.end())
+                mapping[e[0]] = {};
+            if(mapping.find(e[1]) == mapping.end())
+                mapping[e[1]] = {};
+            mapping[e[0]].push_back(e[1]);
+            mapping[e[1]].push_back(e[0]);
+        }
+
+        return (dfs(seen, mapping, -1, 0) && seen.size() == n);
+
+
+    }
+};
