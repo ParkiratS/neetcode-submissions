bac41fcd4e0d1f7388041fc1f23be1dd9befe001
Parkirat Sandhu <85805976+ParkiratS@users.noreply.github.com>
2026-08-19 16:00:52 -0400

    Add: palindrome-partitioning - submission-0

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-0.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+
+    bool isPalindrome(string& s, int l, int r){
+        while(l<=r){
+            if(s[l] != s[r])
+                return false;
+            
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+
+    void traverse(string& s, vector<vector<string>>& ans, vector<string> curr, int l){
+        if(l>=s.size())
+            ans.push_back(curr);
+
+
+        for(int i{l}; i<s.size(); i++){
+            if(isPalindrome(s, l, i)){
+                curr.push_back(s.substr(l,i - l + 1));
+                traverse(s, ans, curr, i+1);
+                curr.pop_back();
+            }
+        }
+    }
+
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+
+        traverse(s, ans, {}, 0);
+        return ans;
+        
+    }
+};
