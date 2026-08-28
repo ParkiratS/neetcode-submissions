2a5418eaa37140fb1e7cbcb0b38a7cfe83cc8a6f
Parkirat Sandhu <85805976+ParkiratS@users.noreply.github.com>
2026-08-28 00:40:29 -0400

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+
+    vector<bool> seen;
+
+    bool traverse(vector<vector<int>>& m, vector<bool>& temp, int num){
+        if(m[num].size() == 0 || seen[num])
+            return true;
+
+        if(temp[num])
+            return false;
+
+        temp[num] = true; 
+        for(int& i:m[num]){
+            if(!traverse(m, temp, i))
+                return false;
+            seen[i] = true;
+        }
+
+        return true;
+    }
+    
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> m(numCourses+1);
+        seen = vector<bool>(numCourses+1, false);
+
+        for(const auto& p:prerequisites)
+            m[p[1]].push_back(p[0]);
+
+        for(int i{0}; i<numCourses; i++){
+            vector<bool> temp(numCourses+1, false);
+            if(!traverse(m, temp, i))
+                return false;
+        }
+
+        return true;
+    }
+};
